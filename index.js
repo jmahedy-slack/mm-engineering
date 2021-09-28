3a060145af56d58f50afde2f7826df7c42b315c3
jmahedy-slack <jmahedy@slack-corp.com>
2021-09-28 14:31:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-heloo worldas
+const https = require('https')
+let url = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html"
+
+
+console.log = "New logging enhancements for MFA";
+
+
+exports.handler = async function(event) {
+  const promise = new Promise(function(resolve, reject) {
+    https.get(url, (res) => {
+        resolve(res.statusCode)
+      }).on('error', (e) => {
+        reject(Error(e))
+      })
+    })
+  return promise
+}
